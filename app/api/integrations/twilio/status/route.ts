b7c777fd3143ee321346cb90fbca552415b16c62
gwcph4q4fd-art <gwcph4q4fd@privaterelay.appleid.com>
2026-06-18 20:23:58 -0400

    Create route.tsAdd Twilio SMS status endpoint

---
+++ app/api/integrations/twilio/status/route.ts
@@ -0,0 +1,9 @@
+import { IntegrationProvider } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { getInboundIntegrationStatus } from "@/lib/inbound-integrations";
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  return NextResponse.json(await getInboundIntegrationStatus(IntegrationProvider.TWILIO));
+}
